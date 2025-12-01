42c3b0461709482d49ce469f5761c1ce12af53e6
jenil-amenity <jenil.z@amenitytech.ai>
2025-12-01 15:10:04 +0530

    Local commit before merge

--- socialbliss/manage.py
+++ socialbliss/manage.py
@@ -15,6 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    finally:
+        "This will definitly executes"
     execute_from_command_line(sys.argv)
 
 
